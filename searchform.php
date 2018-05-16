b199debd7ac5cce1e54dac7d03914a91fe51bf71
jamiebergen <jamie.bergen@gmail.com>
2018-05-15 21:49:43 -0700

    Correct text domain for translation

--- searchform.php
+++ searchform.php
@@ -13,7 +13,7 @@
 
 <form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ) ?>">
 	<label>
-		<span class="screen-reader-text"><?php _ex( 'Search for:', 'label' )?></span>
+		<span class="screen-reader-text"><?php _ex( 'Search for:', 'jmb-sample-theme' )?></span>
 		<input type="search" class="search-field" placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder' ) ?>" value="<?php echo get_search_query() ?>" name="s" />
 	</label>
 	<button type="submit" class="search-submit"><i class="dashicons dashicons-search" aria-hidden="true"></i><span class="screen-reader-text">Submit</span></button>
