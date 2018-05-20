44d510b68a2a81be51eb031ac4e8aadd8e19f109
jamiebergen <jamie.bergen@gmail.com>
2018-05-20 16:24:20 -0700

    Correct translation functions in search form

--- searchform.php
+++ searchform.php
@@ -13,9 +13,12 @@
 
 <form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ) ?>">
 	<label>
-		<span class="screen-reader-text"><?php _ex( 'Search for:', 'jmb-sample-theme' )?></span>
-		<input type="search" class="search-field" placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder' ) ?>" value="<?php echo get_search_query() ?>" name="s" />
+		<span class="screen-reader-text"><?php echo esc_attr_x( 'Search for:', 'label', 'jmb-sample-theme' )?></span>
+		<input type="search" class="search-field" placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder', 'jmb-sample-theme' ) ?>" value="<?php echo get_search_query() ?>" name="s" />
 	</label>
-	<button type="submit" class="search-submit"><i class="dashicons dashicons-search" aria-hidden="true"></i><span class="screen-reader-text">Submit</span></button>
+	<button type="submit" class="search-submit">
+        <i class="dashicons dashicons-search" aria-hidden="true"></i>
+        <span class="screen-reader-text"><?php echo esc_attr_x( 'Submit', 'submit button', 'jmb-sample-theme' ); ?></span>
+    </button>
 </form>
 
